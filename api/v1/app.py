3fa03054b472e44fd62e7069442d79bd3973c5a0
EgyAbdo10 <egyabdo490@gmail.com>
2024-08-05 01:55:13 +0300

    added or modified a file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Flask app main module"""
-from flask import Flask, jsonify
+
+
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -20,7 +22,7 @@ def shutdown(exception=None):
 @app.errorhandler(404)
 def handle_not_found(error):
     """return a not found response"""
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
