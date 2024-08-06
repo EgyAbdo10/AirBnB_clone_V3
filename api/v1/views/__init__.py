a00c11bd7f6b4ca513ee7798c202499e93aba323
EgyAbdo10 <egyabdo490@gmail.com>
2024-08-06 22:01:14 +0300

    added or modified a file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """initialize a blueprint"""
 from flask import Blueprint
-from flask_restful import Api
-from api.v1.views.states import *
+# from flask_restful import Api
 
 app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
+from api.v1.views.states import *
 
-api = Api(app_views)
+# api = Api(app_views)
 
-api.add_resource(State_view_all, "/states/")
-api.add_resource(State_view, "/states/<state_id>")
+# api.add_resource(State_view_all, "/states/")
+# api.add_resource(State_view, "/states/<state_id>")
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """retrieve state objects"""
 from models.state import State
-from flask import jsonify
-from flask_restful import Resource, abort, request
+from flask import jsonify, make_response
+from flask_restful import abort, request
 from models import storage
+from api.v1.views import app_views
 
 
 def abortNotExists(cls, obj_id):
@@ -14,54 +15,127 @@ def abortNotExists(cls, obj_id):
     return obj
 
 
-class State_view_all(Resource):
-    """handle all states requests without id"""
-    def get(self):
-        """get all state objects"""
+@app_views.route("/states", strict_slashes=False, methods=["GET"])
+@app_views.route("/states/<state_id>", strict_slashes=False, methods=["GET"])
+def get_states(state_id=None):
+    """retrieve a state object"""
+    if not state_id:
         objs_dict = storage.all()
         obj_list = []
         for obj in objs_dict.values():
             obj_list.append(obj.to_dict())
-        return jsonify(obj_list)
+        return make_response(jsonify(obj_list))
 
-    def post(self):
-        """Creates a State: POST /api/v1/states"""
-        try:
-            data = request.get_json()
-        except Exception:
-            abort(400, message="Not a JSON")
-        if "name" not in data.keys():
-            abort(400, message="Missing name")
-
-        new_obj = State(**data)
-        storage.new(new_obj)
-        return new_obj.to_dict(), 201
-
-
-class State_view(Resource):
-    """handle all states requests with id"""
-    def get(self, state_id):
-        """Retrieves a State object: GET /api/v1/states/<state_id>"""
+    else:
         obj = abortNotExists(State, state_id)
-        return jsonify(obj.to_dict())
+        return make_response(jsonify(obj.to_dict()))
 
-    def delete(self, state_id):
-        """Deletes a State object:: DELETE /api/v1/states/<state_id>"""
-        obj = abortNotExists(State, state_id)
-        storage.delete(obj)
-        return {}, 200
 
-    def put(self, state_id):
-        """Updates a State object: PUT /api/v1/states/<state_id>"""
-        obj = abortNotExists(State, state_id)
-        try:
-            data = request.get_json()
-        except Exception:
-            abort(400, message="Not a JSON")
+@app_views.route("/states", strict_slashes=False, methods=["POST"])
+def post_states():
+    """create a new state object"""
+    try:
+        data = request.get_json()
+    except Exception:
+        abort(400, message="Not a JSON")
+    if "name" not in data.keys():
+        abort(400, message="Missing name")
 
-        for key, val in data.items():
-            if key not in ["id", "created_at", "updated_at"]:
-                setattr(obj, key, val)
+    new_obj = State(**data)
+    storage.new(new_obj)
+    storage.save()
+    return make_response(new_obj.to_dict(), 201)
 
-        storage.new(obj)
-        return obj.to_dict(), 200
+
+@app_views.route("/states/<state_id>", strict_slashes=False,
+                 methods=["DELETE"])
+def delete_state(state_id):
+    """Deletes a State object:: DELETE /api/v1/states/<state_id>"""
+    obj = abortNotExists(State, state_id)
+    storage.delete(obj)
+    storage.save()
+    return make_response({}, 200)
+
+
+@app_views.route("/states/<state_id>", strict_slashes=False, methods=["PUT"])
+def put_state(state_id):
+    """Updates a State object: PUT /api/v1/states/<state_id>"""
+    obj = abortNotExists(State, state_id)
+    try:
+        data = request.get_json()
+    except Exception:
+        abort(400, message="Not a JSON")
+
+    for key, val in data.items():
+        if key not in ["id", "created_at", "updated_at"]:
+            setattr(obj, key, val)
+
+    storage.save()
+    return make_response(obj.to_dict(), 200)
+
+#     def put(self, state_id):
+#         """Updates a State object: PUT /api/v1/states/<state_id>"""
+#         obj = abortNotExists(State, state_id)
+#         try:
+#             data = request.get_json()
+#         except Exception:
+#             abort(400, message="Not a JSON")
+
+#         for key, val in data.items():
+#             if key not in ["id", "created_at", "updated_at"]:
+#                 setattr(obj, key, val)
+
+#         storage.new(obj)
+#         return obj.to_dict(), 200
+
+# class State_view_all(Resource):
+#     """handle all states requests without id"""
+#     def get(self):
+#         """get all state objects"""
+#         objs_dict = storage.all()
+#         obj_list = []
+#         for obj in objs_dict.values():
+#             obj_list.append(obj.to_dict())
+#         return jsonify(obj_list)
+
+#     def post(self):
+#         """Creates a State: POST /api/v1/states"""
+#         try:
+#             data = request.get_json()
+#         except Exception:
+#             abort(400, message="Not a JSON")
+#         if "name" not in data.keys():
+#             abort(400, message="Missing name")
+
+#         new_obj = State(**data)
+#         storage.new(new_obj)
+#         return new_obj.to_dict(), 201
+
+
+# class State_view(Resource):
+#     """handle all states requests with id"""
+#     def get(self, state_id):
+#         """Retrieves a State object: GET /api/v1/states/<state_id>"""
+#         obj = abortNotExists(State, state_id)
+#         return jsonify(obj.to_dict())
+
+#     def delete(self, state_id):
+#         """Deletes a State object:: DELETE /api/v1/states/<state_id>"""
+#         obj = abortNotExists(State, state_id)
+#         storage.delete(obj)
+#         return {}, 200
+
+#     def put(self, state_id):
+#         """Updates a State object: PUT /api/v1/states/<state_id>"""
+#         obj = abortNotExists(State, state_id)
+#         try:
+#             data = request.get_json()
+#         except Exception:
+#             abort(400, message="Not a JSON")
+
+#         for key, val in data.items():
+#             if key not in ["id", "created_at", "updated_at"]:
+#                 setattr(obj, key, val)
+
+#         storage.new(obj)
+#         return obj.to_dict(), 200
